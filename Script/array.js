8fb665c0a5c416e0ef7105241272eda506f13587
Dio Brando <7iscool77@gmail.com>
2018-01-31 13:41:47 +0100

    added more music
    removed a useless file

--- Script/array.js
+++ Script/array.js
@@ -14,6 +14,23 @@ var museArray = [
   mus12 = {title: "Starshine", artist: "Gorillaz", album: "Gorillaz", release: "2001", mood:"placeholder"},
   mus13 = {title: "Slow Country", artist: "Gorillaz", album: "Gorillaz", release: "2001", mood:"placeholder"},
   mus14 = {title: "M1 A1", artist: "Gorillaz", album: "Gorillaz", release: "2001", mood:"placeholder"},
+  mus15 = {title: "Daftendirekt", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus16 = {title: "Wdpk 837 FM", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus17 = {title: "Revolution 909", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus18 = {title: "Da Funk", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus19 = {title: "Phoenix", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus20 = {title: "Fresh", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus21 = {title: "Around the World", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus22 = {title: "Rollin' & Scratchin'", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus23 = {title: "Teachers", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus24 = {title: "High Fidelity", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus25 = {title: "Rock'n Roll", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus26 = {title: "Oh Yeah", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus27 = {title: "Burnin'", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus28 = {title: "Indo Silver Club", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus29 = {title: "Alive", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+  mus30 = {title: "Funk Ad", artist: "Daft Punk", album: "Homework", release: "1997", mood:"placeholder"},
+
 ];
 
 // Moods: Amused, Blissful, Calm, Cheerful, Dreamy, Energetic, Excited, Happy, Loving, Mellow, Peaceful
